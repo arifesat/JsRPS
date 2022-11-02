4853427fb8e6465b1f40a8147578c4528c64fb80
arifesat <arif_esat@outlook.com>
2022-11-02 15:26:17 +0300

    Created first files and created a function that gives a random option to computer

---
+++ script.js
@@ -0,0 +1,14 @@
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))];
+}
+var options = ["rock", "paper", "scissors"]
+function getComputerChoice() {
+    let computerSelection = options.random()
+
+}
+function playRound(playerSelection, computerSelection) {
+    
+}
+const playerSelection = prompt("What is your play?\nRock\nPaper\nScissors");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));

e22c8df93c04f63d3ab5ab8c30927524dde92306
arifesat <arif_esat@outlook.com>
2022-11-03 20:15:18 +0300

    Finished project

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 Array.prototype.random = function () {
     return this[Math.floor((Math.random()*this.length))];
-}
+} // selects one random option, found this function on stackoverflow
+
 var options = ["rock", "paper", "scissors"];
 
-function getComputerChoice() {
-    let computerSelection = options.random();
-}
-function playRound(playerSelection, computerSelection) {
+let playerScore = 0;
+let computerScore = 0;
 
-const playerSelection = prompt("What is your play?\nRock\nPaper\nScissors");
-const computerSelection = getComputerChoice();
-playRound()
+
+function playRound() {
+    let computerSelection = options.random();
+    let playerSelection = prompt("What is your play?\nRock\nPaper\nScissors");
+
+    if ((playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "rock" && computerSelection == "scissors")) {
+        playerScore += 1;
+        console.log("You won this round!")
+    }
+    else if (playerSelection == computerSelection) {
+        playerScore += 1;
+        computerScore += 1;
+        console.log("It's a tie!");
+    }
+    else if ((computerSelection == "paper" && playerSelection == "rock") || (computerSelection == "scissors" && playerSelection == "paper") || (computerSelection == "rock" && playerSelection == "scissors")) {
+        computerScore += 1;
+        console.log("Computer won this round :(");
+    }
+    else {
+        console.log("Enter a valid option")
+    }
+    }
+
+    if (computerScore == 5) {
+        console.log("Computer wins! You lost :(")
+    }
+    else if (playerScore == 5) {
+        console.log("You won! Congrats!")
+    }
+
+while ((playerScore < 5) && (computerScore < 5)) {
+    playRound();
+}
